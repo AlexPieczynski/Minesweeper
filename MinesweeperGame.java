da8751ab23005a9e7613756ba788f745d30a9b22
Alex Pieczynski <apiecz2@uic.edu>
2016-02-09 17:17:53 -0600

    Created the class for a Minesweeper game. ---contains some basic data members for keeping track of the game's state ---contains some simple test code for the timer which will print a number every second ----------considering putting the timer in its own class

---
+++ MinesweeperGame.java
@@ -0,0 +1,40 @@
+//This class is used to instantiate a game of Minesweeper
+//Run this files main() begin playing
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class MinesweeperGame
+{
+  private int nMines; //used to display num of mines left on the board. decrements when a mine is marked.
+  private Timer clock; //counts time elapsed since first left-click on Grid
+  private int secs; //same as above
+  
+  //private Grid board;
+  
+  //updates secs value every time clock fires. also prints out each second. STOPS AT 15 SECONDS
+  private class TimeHandler implements ActionListener{
+    public void actionPerformed( ActionEvent event ){
+      secs++;
+      if (secs < 15)
+        System.out.println(secs);
+      else
+        clock.stop();
+    }
+  }
+  
+  public MinesweeperGame()
+  {
+    nMines = 10;
+    secs = 0;
+    TimeHandler t = new TimeHandler(); 
+    clock = new Timer(1000, t);
+    clock.start();
+  }
+  
+  public static void main (String[] args)
+  {
+    MinesweeperGame game = new MinesweeperGame();
+  }
+}
